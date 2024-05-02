7da688a71b38db923abaf4403e15d4bea43c23cd
goameliagoh <s10257856@connect.np.edu.sg>
2024-05-02 23:08:36 +0800

    COMPLETED

--- Week_03/books-api-mvc/app.js
+++ Week_03/books-api-mvc/app.js
@@ -13,24 +13,32 @@
 // Import the modules needed
 const express = require("express");
 const bodyParser = require("body-parser");
-const booksController = require("./controllers/booksController"); // Import controllers
+const booksController = require("./controllers/booksController"); // Import controller so that can even interact with the database in the 1st place.
 
 // Initialize Express App
 const app = express();
 
-// Using Middleware [ use() ] !!
+// Using Middleware [ use() ] , MUST ALWAYS HAVE THIS IF YOURE ACCEPTING USER INPUT !!
+// ---> recap: middleware will read the user input and put in req.body <------
 app.use(bodyParser.json()); // Parse incoming JSON data in request body
 app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
 
 // Define individual routes for each controller function
-app.get("/books", booksController.getAllBooks);
+app.get("/books", booksController.getAllBooks); // Eg if user sends GET request with /books, then booksController.getAllBooks function is called out.
 app.get("/books/:id", booksController.getBookById);
 app.post("/books", booksController.createBook);
 app.put("/books/:id", booksController.updateBook);
 app.delete("/books/:id", booksController.deleteBook);
 
 // Defining port + Starting server
-const port = process.env.PORT || 3000;
+// ------------------------ EXPLAINING CODE BELOW --------------------------------------
+// 'process.env' is like a secret book containing a list of things
+// 'process.env.PORT' == you are searching for whether PORT exists in that secret book 
+// if exists --> you just set the 'const port' as the 'PORT' value
+// if dont exists --> you set 'const port' to its default aka 3000
+//----------------------------------------------------------------------------------------
+const port = process.env.PORT || 3000; 
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- Week_03/books-api-mvc/middlewares/validateBook.js
+++ Week_03/books-api-mvc/middlewares/validateBook.js
@@ -1,21 +1,25 @@
 //This file will house the validation logic using Joi.
 
 //npm install joi INSIDE THE TERMINAL
-
 const Joi = require("joi");
 
-const validateBook = (req, res, next) => {
+const validateBook = (req, res, next) => { // 'next' is passing from 1 middleware to another middleware 
+  
+  //-------------------------------------------------------------------------------------------------
+  // Conditions that book info must fulfill to be able to be validated!!!
   const schema = Joi.object({
     title: Joi.string().min(3).max(50).required(),
     author: Joi.string().min(3).max(50).required(),
   });
+  //----------------------------------------------------------------------------------------------------
 
-  const validation = schema.validate(req.body, { abortEarly: false }); // Validate request body
+  const validation = schema.validate(req.body, { abortEarly: false }); // Saying: Based on the schema aka conditions I created above, I will refer to it to validate my req.body. + I will validate the data's title and author and not validate halfway (thats why abortEarly = false)
+  // 'validate' is a JOI method ++++ 'validation' output will be the details of those data that are NOT validated.
 
-  if (validation.error) {
-    const errors = validation.error.details.map((error) => error.message);
+  if (validation.error) { // <--- write this to check if hv any validation error...
+    const errors = validation.error.details.map((error) => error.message); // Basically: 'validation.error.details' contains all details about your error ++ 'map((error)=>error.message)' will make sure that the correct type of error is mapped tgt with the details abt the error
     res.status(400).json({ message: "Validation error", errors });
-    return; // Terminate middleware execution on validation error
+    return; // Terminate middleware execution on validation error so that client will be aware of validation error before processing further ++ This is referring to my WHOLE DATASET and hence different from abortEarly = false (???)
   }
 
   next(); // If validation passes, proceed to the next route handler
